cdcbc7e3cd6c9feb16021e6fbd3006072a67b743
sarah.jones99@aggiemail.usu.edu <sarahjones@Sarahs-MacBook-Pro-2.local>
2022-11-19 21:40:16 -0700

    first commit

---
+++ WidgetRequests.py
@@ -0,0 +1,12 @@
+import boto3
+
+class WidgetRequests:
+
+    def post_update(self, body):
+        print("hi")
+
+    def post_create(self, body):
+        print("hi")
+
+    def post_delete(self, body):
+        print("hi")
---
+++ test_widgetRequests.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestWidgetRequests(TestCase):
+    def test_post_update(self):
+        self.fail()
+
+    def test_post_create(self):
+        self.fail()
+
+    def test_post_delete(self):
+        self.fail()
