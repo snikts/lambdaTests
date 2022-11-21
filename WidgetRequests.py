3a73c354623f5f106c4bbf21e7e3a9fe8d2ae59a
sarah.jones99@aggiemail.usu.edu <sarahjones@Sarahs-MacBook-Pro-2.local>
2022-11-20 18:28:42 -0700

    tests finished

--- WidgetRequests.py
+++ WidgetRequests.py
@@ -1,12 +1,28 @@
 import boto3
+import json
 
 class WidgetRequests:
 
     def post_update(self, body):
-        print("hi")
+        lambda_client = boto3.client('lambda')
+        lambda_payload = body
+        response = lambda_client.invoke(FunctionName='widget-request-handler',
+                             InvocationType='Event',
+                             Payload=json.dumps(lambda_payload))
+        return response
 
     def post_create(self, body):
-        print("hi")
+        lambda_client = boto3.client('lambda')
+        lambda_payload = body
+        response = lambda_client.invoke(FunctionName='widget-request-handler',
+                             InvocationType='Event',
+                             Payload=json.dumps(lambda_payload))
+        return response
 
     def post_delete(self, body):
-        print("hi")
+        lambda_client = boto3.client('lambda')
+        lambda_payload = body
+        response = lambda_client.invoke(FunctionName='widget-request-handler',
+                             InvocationType='Event',
+                             Payload=json.dumps(lambda_payload))
+        return response
--- test_widgetRequests.py
+++ test_widgetRequests.py
@@ -1,12 +1,112 @@
 from unittest import TestCase
+from WidgetRequests import WidgetRequests
+import boto3
+import json
 
 
 class TestWidgetRequests(TestCase):
+    def setUp(self):
+        self.wr = WidgetRequests()
+        self.sqs = boto3.resource('sqs')
+        self.queue = self.sqs.get_queue_by_name(QueueName='request-queue')
+
     def test_post_update(self):
-        self.fail()
+        body = {
+            "requestType": "update",
+            "requestId": "38d51e39-c470-462b-a72b-cfd04335454e",
+            "widgetId": "ad0bb9e1-28e9-46e0-ad08-8192f4d3b6c6",
+            "owner": "John Jones",
+            "label": "",
+            "description": "QQGRLNZY",
+            "otherAttributes": [
+                {
+                    "name": "color",
+                    "value": "red"
+                },
+                {
+                    "name": "size-unit",
+                    "value": "cm"
+                },
+                {
+                    "name": "height",
+                    "value": "955"
+                },
+                {
+                    "name": "width-unit",
+                    "value": "cm"
+                },
+                {
+                    "name": "length",
+                    "value": "247"
+                },
+                {
+                    "name": "price",
+                    "value": "71.11"
+                },
+                {
+                    "name": "vendor",
+                    "value": "UPAZOD"
+                },
+                {
+                    "name": "note",
+                    "value": "MUUDBUWKGVFCXPEUSKBBBDEYQTZDNWCPPGHFBXPILELOVETUGRTWMEMZMLKUEMAEOZTSMAMNFPISAMWFYZJHDQQSOIPOGFJY"
+                }
+            ]
+        }
+        r = self.wr.post_create(body)
+        r = r['ResponseMetadata']
+        r = r['HTTPStatusCode']
+        itemFound = False
+        if(r == 202):
+            itemFound = True
+        self.assertEqual(itemFound, True)
 
     def test_post_create(self):
-        self.fail()
+        body = {
+            "requestType": "create",
+            "requestId": "e80fab52-71a5-4a76-8c4d-11b66b83ca2a",
+            "widgetId": "8123f304-f23f-440b-a6d3-80e979fa4cd6",
+            "owner": "Mary Matthews",
+            "label": "JWJYY",
+            "description": "THBRNVNQPYAWNHGRGUKIOWCKXIVNDLWOIQTADHVEVMUAJWDONEPUEAXDITDSHJTDLCMHHSESFXSDZJCBLGIKKPUYAWKQAQI",
+            "otherAttributes": [
+                {
+                    "name": "width-unit",
+                    "value": "cm"
+                },
+                {
+                    "name": "length-unit",
+                    "value": "cm"
+                },
+                {
+                    "name": "rating",
+                    "value": "2.580677"
+                },
+                {
+                    "name": "note",
+                    "value": "FEGYXHIJCTYNUMNMGZBEIDLKXYFNHFLVDYZRNWUDQAKQSVFLPRJTTXARVEIFDOLTUSWZZWVERNWPPOEYSUFAKKAPAGUALGXNDOVPNKQQKYWWOUHGOJWKAJGUXXBXLWAKJCIVPJYRMRWMHRUVBGVILZRMESQQJRBLXISNFCXGGUFZCLYAVLRFMJFLTBOTLKQRLWXALLBINWALJEMUVPNJWWRWLTRIBIDEARTCSLZEDLZRCJGSMKUOZQUWDGLIVILTCXLFIJIULXIFGRCANQPITKQYAKTPBUJAMGYLSXMLVIOROSBSXTTRULFYPDFJSFOMCUGDOZCKEUIUMKMMIRKUEOMVLYJNJQSMVNRTNGH"
+                }
+            ]
+        }
+        r = self.wr.post_create(body)
+        r = r['ResponseMetadata']
+        r = r['HTTPStatusCode']
+        itemFound = False
+        if(r == 202):
+            itemFound = True
+        self.assertEqual(itemFound, True)
 
     def test_post_delete(self):
-        self.fail()
+        body = {
+            "type":"delete",
+            "requestId":"21cf74b2-dbf2-46bb-a274-b8e3eac679bf",
+            "widgetId":"e50b4381-4332-438f-bcec-bd2b8c0fa5ed",
+            "owner":"Sue Smith"
+        }
+        r = self.wr.post_create(body)
+        r = r['ResponseMetadata']
+        r = r['HTTPStatusCode']
+        itemFound = False
+        if(r == 202):
+            itemFound = True
+        self.assertEqual(itemFound, True)
